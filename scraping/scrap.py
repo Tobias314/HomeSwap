75c13c3ad0fc4480c99c1967627002432f7e90d4
StackNeverFlow <50374560+StackNeverFlow@users.noreply.github.com>
2024-04-05 17:49:35 +0200

    Update * (first stage of scraping)

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -1,15 +1,16 @@
-import requests, time
+import requests
+import time
 from bs4 import BeautifulSoup
 
-Max_Resultes = 1752 -1
+Max_Resultes = 1752
 
 cookies = {
     "tauschwohnung": "e80188375b1d0be651091a1874b5fd53f05427d9"
 }
 
 for x in range(Max_Resultes):
-    x=x+1
-    print(x)
+    x = x + 1
+    print(f"Scraping site {x} of {Max_Resultes}")
     url = f"https://www.tauschwohnung.com/search/result?city=Berlin&housing_type=1&storey_min=-1&storey_max=100&sort=standard&page={x}"
     response = requests.get(url, cookies=cookies)
 
@@ -17,12 +18,19 @@ for x in range(Max_Resultes):
         soup = BeautifulSoup(response.text, 'html.parser')
         listing_links = soup.find_all('a', class_='stretched-link')
         listing_urls = [link['href'] for link in listing_links]
+        listing_urls = list(set(listing_urls))
         print("Listing URLs found:", listing_urls)
 
         for item in listing_urls:
             item_response = requests.get("https://www.tauschwohnung.com/" + item, cookies=cookies)
-            item_soup = BeautifulSoup(response.text, 'html.parser')
-            
+            item_soup = BeautifulSoup(item_response.text, 'html.parser')
+
             item_card = item_soup.find_all('div', class_='d-none d-sm-block')
             item_element = item_soup.find_all('div', class_='col-6 col-md-2 mb-2')
-            print(item_element)
+            
+            extracted_item = {}
+            for element in item_element:
+                key = element.find('br').previous_sibling.strip()
+                value = element.find('strong').text.strip().replace("\xa0", "")
+                extracted_item[key] = value
+            print("Extracted item:", extracted_item)
