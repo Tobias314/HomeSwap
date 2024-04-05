df842e924d7e5b47392ba635e84196a2462134dc
Zero <susann.janetzki@student.hpi.uni-potsdam.de>
2024-04-06 00:32:55 +0200

    better offer page

--- frontend/lib/pages/offer_page.dart
+++ frontend/lib/pages/offer_page.dart
@@ -1,4 +1,5 @@
 import 'package:dating_app/pages/confetti_page.dart';
+import 'package:dating_app/pages/swipe_page.dart';
 import 'package:flutter/material.dart';
 
 // Define a data class for the image data
@@ -22,50 +23,81 @@ class OfferPage extends StatelessWidget {
     // Define a list of ImageData objects
     List<ImageData> imageDataList = [
       ImageData(
-        imagePath: "assets/images/example1.jpg",
-        name: "Angebot 1",
-        description: "Zimmer in 2er WG in Berlin Neukölln, 20 m²",
-      ),
-      ImageData(
         imagePath: "assets/images/example2.jpg",
-        name: "Angebot 2",
-        description: "Wohnung in Berlin Moabit, 55 m²",
-      ),
-      ImageData(
-        imagePath: "assets/images/example3.jpg",
-        name: "Angebot 3",
-        description: "Appartment in Berlin Friedrichshain, 45 m²",
+        name: "Wohnung in Berlin Moabit, 55 m²",
+        description: "3 Zimmer, 800€ Kaltmiete",
       ),
     ];
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wir haben ein paar ein paar Matches für dich!'),
+        title: Text('Wir haben ein paar ein Match für dich!'),
       ),
       body: ListView.separated(
         itemCount: imageDataList.length,
         separatorBuilder: (BuildContext context, int index) => const Divider(),
         itemBuilder: (BuildContext context, int index) {
           final imageData = imageDataList[index];
-          return ListTile(
-            leading: Image.asset(imageData.imagePath),
-            title: Text(imageData.name),
-            subtitle: Text(imageData.description),
-            trailing: ElevatedButton(
-              onPressed: () {
-                // Add your logic for "Match akzeptieren" button press here
-                print('Match akzeptieren pressed for ${imageData.name}');
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ConfettiPage()),
-                );
-              },
-              child: Text('Match akzeptieren'),
-            ),
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                imageData.imagePath,
+                fit: BoxFit.cover, // Ensure the image covers the entire area
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      imageData.name,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(imageData.description),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your logic for "Match akzeptieren" button press here
+                    print('Match akzeptieren pressed for ${imageData.name}');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ConfettiPage()),
+                    );
+                  },
+                  child: Text('Match akzeptieren'),
+                ),
+              ),
+              SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your logic for "Match akzeptieren" button press here
+                    print('Match abgelehnt pressed for ${imageData.name}');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SwipePage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red, // Set the button color to red
+                  ),
+                  child: Text('Match ablehnen'),
+                ),
+              ),
+            ],
           );
         },
       ),
     );
   }
 }
-
