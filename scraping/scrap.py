8d76bd21335596c9714e2affd77d1c941b369f3e
StackNeverFlow <50374560+StackNeverFlow@users.noreply.github.com>
2024-04-05 17:22:16 +0200

    Update scrap.py (extract item per entry)

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -1,21 +1,28 @@
-import requests
+import requests, time
 from bs4 import BeautifulSoup
+
+Max_Resultes = 1752 -1
 
 cookies = {
     "tauschwohnung": "e80188375b1d0be651091a1874b5fd53f05427d9"
 }
 
-def initial_request():
-    url = "https://www.tauschwohnung.com/search/result?city=Berlin&housing_type=1&storey_min=-1&storey_max=100&sort=standard&page=1"
+for x in range(Max_Resultes):
+    x=x+1
+    print(x)
+    url = f"https://www.tauschwohnung.com/search/result?city=Berlin&housing_type=1&storey_min=-1&storey_max=100&sort=standard&page={x}"
     response = requests.get(url, cookies=cookies)
 
     if response.status_code == 200:
-        print(response.text)
         soup = BeautifulSoup(response.text, 'html.parser')
+        listing_links = soup.find_all('a', class_='stretched-link')
+        listing_urls = [link['href'] for link in listing_links]
+        print("Listing URLs found:", listing_urls)
 
-        page_links = soup.find_all('a', class_='page-link')
-
-        page_numbers = [link['href'].split('=')[-1] for link in page_links]
-        print(page_numbers)
-    else:
-        print("Failed to retrieve the webpage. Status code:", response.status_code)
+        for item in listing_urls:
+            item_response = requests.get("https://www.tauschwohnung.com/" + item, cookies=cookies)
+            item_soup = BeautifulSoup(response.text, 'html.parser')
+            
+            item_card = item_soup.find_all('div', class_='d-none d-sm-block')
+            item_element = item_soup.find_all('div', class_='col-6 col-md-2 mb-2')
+            print(item_element)
