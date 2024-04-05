abe282cb330865b57edf43a49a9486ac798072a8
StackNeverFlow <50374560+StackNeverFlow@users.noreply.github.com>
2024-04-05 21:17:25 +0200

    Update *

---
+++ scraping/clean_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def start():
+    df = pd.read_csv("new.csv", index_col="ID")
+
+    def clean_and_convert(value):
+        cleaned_value = value.replace('€', '').replace('m²', '').replace(".", "").replace('"', "").replace(",", ".").strip()  # Remove '€', 'm²' and strip leading/trailing spaces
+        try:
+            return float(cleaned_value)
+        except ValueError:
+            print("Problematic value:", value)
+            return None
+
+    df['offer_price'] = df['offer_price'].apply(clean_and_convert)
+    df['offer_size'] = df['offer_size'].apply(clean_and_convert)
+    df['offer_rooms'] = df['offer_rooms'].apply(clean_and_convert)
+    df['pref_max_price'] = df['pref_max_price'].apply(clean_and_convert)
+    df['pref_min_size'] = df['pref_min_size'].apply(clean_and_convert)
+    df['pref_min_rooms'] = df['pref_min_rooms'].apply(clean_and_convert)
+
+    df.to_csv("filtered.csv")
+    
+    import csv_to_json
+    csvFilePath = r'filtered.csv'
+    jsonFilePath = r'data.json'
+    csv_to_json.csv_to_json(csvFilePath, jsonFilePath)
--- scraping/scrap.py
+++ scraping/scrap.py
@@ -1,6 +1,6 @@
-import requests
-import time
+import requests, time, json, re
 from bs4 import BeautifulSoup
+import pandas as pd
 
 Max_Resultes = 1752
 
@@ -8,7 +8,12 @@ cookies = {
     "tauschwohnung": "e80188375b1d0be651091a1874b5fd53f05427d9"
 }
 
-for x in range(Max_Resultes):
+def extract_int(value):
+    return re.findall(r'\d+', value)
+
+df = pd.DataFrame()
+
+for x in range(10):
     x = x + 1
     print(f"Scraping site {x} of {Max_Resultes}")
     url = f"https://www.tauschwohnung.com/search/result?city=Berlin&housing_type=1&storey_min=-1&storey_max=100&sort=standard&page={x}"
@@ -23,6 +28,7 @@ for x in range(Max_Resultes):
 
         for item in listing_urls:
             item_response = requests.get("https://www.tauschwohnung.com/" + item, cookies=cookies)
+            
             item_soup = BeautifulSoup(item_response.text, 'html.parser')
 
             item_card = item_soup.find_all('div', class_='d-none d-sm-block')
@@ -33,7 +39,7 @@ for x in range(Max_Resultes):
                 key = element.find('br').previous_sibling.strip()
                 value = element.find('strong').text.strip().replace("\xa0", "")
                 extracted_item[key] = value
-            #print("Extracted item:", extracted_item)
+            print("Gesucht:", extracted_item)
 
             item_characteristics = item_soup.find('div', class_='table-responsive')
             characteristics_table = item_characteristics.find('table', class_='table-characteristics')
@@ -46,4 +52,44 @@ for x in range(Max_Resultes):
                 characteristic_value = cells[1].text.strip().replace("                ", "")
                 extracted_characteristics[characteristic_name] = characteristic_value
             
-            #print("Extracted characteristics:", extracted_characteristics)
+            print("Extracted characteristics:", extracted_characteristics)
+
+            total = extracted_characteristics | extracted_item
+            r = json.dumps(total, ensure_ascii=False).encode('utf8')
+            #print(r)
+
+            item_details = item_soup.find_all('div', class_='col-4')
+            
+            extracted_details = {}
+            for detail in item_details:
+                strong_tag = detail.find('strong')
+                if strong_tag:
+                    value = strong_tag.text.strip()
+                    key = strong_tag.find_next_sibling('span').text.strip()
+                    extracted_details[key] = value
+
+            print("Item details:", extracted_details)
+
+            articel_id = item_soup.find('p', 'grey mb-0 pb-0').find('small').text.strip()
+            articel_id = extract_int(articel_id)[0]
+
+            image_url = item_soup.find("img", "embed-responsive-item d-block w-100").get("src")
+
+            extracted_item["img_url"] = "https://www.tauschwohnung.com" + image_url
+
+            # df = pd.DataFrame([extracted_details, extracted_characteristics, extracted_item])
+            # print(df)
+            out = extracted_details | extracted_characteristics | extracted_item
+            print(out)
+            #df.append(out, index=[articel_id])#index = ["Kaltmiete", "Wohnfläche", "Zimmer", "Antwortrate", "Antwortzeit", "Letzter Login", "Objektart", "Nebenkosten", "Kaution", "Stockwerk", "Außenausstattung", "Innenausstattung", "Fußboden", "Heizung", "Merkmale", "Wohnung", "Gesucht Stadt", "Gesucht Stadtteile", "Gesucht Kaltmiete (max.)", "Gesucht Wohnfläche (min.)", "Gesucht Zimmer (min.)"]
+            df = pd.concat([df, pd.DataFrame(out, index=[articel_id])])
+            df = df.rename_axis('ID')
+
+df.to_csv("t.csv")
+df.to_json("t.json")
+df = df.drop(["Antwortrate", "Antwortzeit", "Letzter Login", "Objektart", "Nebenkosten", "Kaution", "Einzugsdatum", "Stockwerk", "Außenausstattung", "Innenausstattung", "Fußboden", "Heizung", "Merkmale", "Wohnung", "Stadt", "Stadtteile", "PLZ"], axis=1)
+df = df.rename(columns={"Kaltmiete (max.)": "pref_max_price", "Wohnfläche (min.)": "pref_min_size", "Zimmer (min.)": "pref_min_rooms"})
+df = df.rename(columns={"Kaltmiete": "offer_price", "Wohnfläche": "offer_size", "Zimmer": "offer_rooms"})
+df.to_csv("new.csv")
+import clean_csv
+clean_csv.start()
