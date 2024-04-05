787e24aac53f685628224a093e3916a25e72504f
Zero <susann.janetzki@student.hpi.uni-potsdam.de>
2024-04-05 19:38:01 +0200

    kewskjbsdbfk

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,24 +1,89 @@
+// https://www.tutorialkart.com/flutter/flutter-login-screen/#gsc.tab=0
+
 import 'package:flutter/material.dart';
-import 'pages/preference_page.dart';
+import 'pages/profile_page.dart';
+import 'pages/swipe_page.dart';
 
-void main() => runApp(MyApp());
-
+void main() => runApp(const MyApp());
+ 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  static const String _title = 'Home Ring Swapping App';
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Settings UI Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        brightness: Brightness.light,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
       ),
-      darkTheme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        accentColor: Colors.deepPurple,
-        brightness: Brightness.dark,
-      ),
-      home: SettingsScreen(),
     );
+  }
+}
+ 
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+ 
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+ 
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                ),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Please sign in',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Sign In'),
+              onPressed: () {
+                print(nameController.text);
+                print(passwordController.text);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SwipePage()),
+              );
+          },
+        ),
+          ],
+        ));
   }
 }
