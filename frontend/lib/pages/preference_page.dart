b42103867f528b801036b96068d1944f847aabc5
Zero <susann.janetzki@student.hpi.uni-potsdam.de>
2024-04-05 18:59:27 +0200

    Add text size slider to settings page

--- frontend/lib/pages/preference_page.dart
+++ frontend/lib/pages/preference_page.dart
@@ -8,32 +8,47 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   bool lockInBackground = true;
+  double userTextSize = 0.5;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Settings UI')),
-      body:  SettingsList(
-      sections: [
-        SettingsSection(
-          title: Text('Common'),
-          tiles: <SettingsTile>[
-            SettingsTile.navigation(
-              leading: Icon(Icons.language),
-              title: Text('Language'),
-              value: Text('English'),
-            ),
-            SettingsTile.switchTile(
-              onToggle: (value) {},
-              initialValue: true,
-              leading: Icon(Icons.format_paint),
-              title: Text('Enable custom theme'),
-            ),
-          ],
-        ),
-      ],
-    ),
-             
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: Icon(Icons.language),
+                title: Text('Language'),
+                value: Text('English'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: Icon(Icons.format_paint),
+                title: Text('Enable custom theme'),
+              ),
+              SettingsTile(
+                title: Text('Text Size'),
+                leading: Icon(Icons.text_fields),
+                trailing: Slider(
+                  min: 0.5,
+                  max: 2.0,
+                  divisions: 3,
+                  value: userTextSize,
+                  onChanged: (newValue) {
+                    setState(() {
+                      userTextSize = newValue;
+                    });
+                  },
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
